adc9bdfdaea21c723d0a2e87afcb58482e024535
megaturbo <megaturbo@truelevel.io>
2019-07-11 15:16:00 +0200

    Clean and fix tests

--- test_ts/atola.ts
+++ test_ts/atola.ts
@@ -1,12 +1,29 @@
-const AtolaTruffle = artifacts.require("Atola");
+const AtolaArtifacts = artifacts.require("Atola");
 import { Atola } from './types/Atola';
+import { Eth } from 'web3x/eth';
 const config = (process.env.NODE_ENV === 'production')
   ? require('../config/ropsten.json')
   : require('../config/local.json')
 
 contract('Atola', (accounts) => {
+  let eth;
+  let atola: Atola;
+
+  before(async () => {
+    // retrieve current provider
+    const currentEth = Eth.fromCurrentProvider();
+    if (currentEth) {
+      eth = currentEth;
+    } else {
+      throw Error('No web3 provider found');
+    }
+    const atolaAddress = (await AtolaArtifacts.deployed()).address;
+
+    // retrieve deployed contracts
+    atola = new Atola(eth, atolaAddress);
+  });
+
   it('verify baseexchange address', async () => {
-    const atola: Atola = (await AtolaTruffle.deployed()).contract;
     const baseExchangeToken = await atola.methods.baseexchange().call();
     assert.equal(baseExchangeToken, config.UNISWAP_EXCHANGE, "baseexchange address is not the same in the deployed contract than in the generated config file");
   });
--- test_ts/exchange.ts
+++ test_ts/exchange.ts
@@ -1,45 +1,50 @@
-const UniswapExchangeArtifacts = artifacts.require("uniswap_exchange");
 import { UniswapExchangeInterface } from './types/UniswapExchangeInterface';
-import BN from 'bn.js';
-import { toWei } from 'web3x/utils';
+import { ERC20 } from './types/ERC20';
+import { fromWei, toBN } from 'web3x/utils';
 import { Address } from 'web3x/address';
+import { Eth } from 'web3x/eth';
 
 const config = (process.env.NODE_ENV === 'production')
   ? require('../config/ropsten.json')
   : require('../config/local.json')
 
+
+
 contract('Uniswap Exchange', (accounts) => {
-  it('checks that totalSupply is at 0', async () => {
-    const exchange: UniswapExchangeInterface = (await UniswapExchangeArtifacts.deployed()).contract;
-    const totalSupply = await exchange.methods.totalSupply().call();
-    console.log(totalSupply);
-    assert.equal(totalSupply, totalSupply, 'total supply is not 0');
+  let eth: Eth;
+  let exchange: UniswapExchangeInterface;
+  let exchangeAddress: Address;
+
+  before(() => {
+    // retrieve current provider
+    const currentEth = Eth.fromCurrentProvider();
+    if (currentEth) {
+      eth = currentEth;
+    } else {
+      throw Error('No web3 provider found');
+    }
+
+    // retrieve deployed contracts
+    exchange = new UniswapExchangeInterface(eth, config.UNISWAP_EXCHANGE);
+
+    // params
+    if(exchange.address) {
+      exchangeAddress = exchange.address;
+    }
+
+    // various checks
+    if(!exchangeAddress) {
+      throw Error('Exchange address not set');
+    }
   });
 
-  it('can add liquidity', async () => {
-    // const exchange: UniswapExchangeInterface = (await UniswapExchangeArtifacts.deployed()).contract;
-    console.log(await UniswapExchangeArtifacts.deployed());
-    const exchange: UniswapExchangeInterface = new UniswapExchangeInterface(web3.eth, (await UniswapExchangeArtifacts.deployed()).address);
+  it('checks factory address', async () => {
+    const factoryAddress = await exchange.methods.factoryAddress().call();
+    assert.equal(factoryAddress, config.UNISWAP_FACTORY, 'factory address is wrong');
+  });
 
-    // add liquidity to exchange
-    const minLiquidity = new BN(0);   // we don't care since total_liquidity will be 0
-    const maxTokens = new BN(1000);   // 1000 tokens for 1 ETH
-    const deadline = new BN("1569732084");
-    const value = toWei("1", "ether");
-    const from = Address.fromString(accounts[0]);
-
-    // .send() is definitely waiting for a string and not an Address
-    const tx = await exchange.methods.addLiquidity(
-      minLiquidity.toString(),
-      maxTokens.toString(),
-      deadline.toString()
-    ).send({
-      from: from,
-      value: value
-    });
-    const txHash = await tx.getTxHash();
-    const receipt = await tx.getReceipt();
-
-    assert.equal(true, true, 'true fucked');
+  it('checks token address', async () => {
+    const tokenAddress = await exchange.methods.tokenAddress().call();
+    assert.equal(tokenAddress, config.BASE_TOKEN, 'token address is wrong');
   });
 })
