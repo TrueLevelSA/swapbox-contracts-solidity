0b89bce5ec60fc5d02a6f23ee52c73f4cd90d010
megaturbo <megaturbo@truelevel.io>
2019-07-17 14:45:05 +0200

    Add test to check initial token and eth balances of exchanges

--- test_ts/pricefeed.ts
+++ test_ts/pricefeed.ts
@@ -3,6 +3,7 @@ const AtolaArtifacts = artifacts.require("Atola");
 import { PriceFeed } from './types/PriceFeed';
 import { Atola } from './types/Atola';
 import { Eth } from 'web3x/eth';
+import { fromWei } from 'web3x/utils';
 
 contract('PriceFeed', (accounts) => {
   let eth;
@@ -25,11 +26,20 @@ contract('PriceFeed', (accounts) => {
     priceFeed = new PriceFeed(eth, priceFeedAddress);
   });
 
-  it('priceFeedAddress', async () => {
+  it('get correct balances', async () => {
     const balances = await priceFeed.methods.getBalances().call();
-    // const supportedTokens = await atola.methods.supportedTokensArr().call();
-    const addresses = balances.map(b => b[0].toString());
-    console.log(addresses);
-    assert.equal(true, true, "TODO");
+
+    const addresses = balances[0].map(address => address.toString());
+    const tokenBalances = balances[1];
+    const ethBalances = balances[2].map(
+      ethBalance => fromWei(ethBalance, "ether")
+    );
+
+    // TODO: use a config file for default setup of liquidity
+    assert.equal(tokenBalances[0], '1000', 'tokenBalance is wrong in baseToken');
+    assert.equal(tokenBalances[1], '1000', 'tokenBalance is wrong in scndToken');
+
+    assert.equal(ethBalances[0], '1', 'ethBalances is wrong for baseToken');
+    assert.equal(ethBalances[1], '1', 'ethBalances is wrong for scndToken');
   });
 });
