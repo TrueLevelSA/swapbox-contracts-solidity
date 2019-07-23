8347d5dd1173a5a963e44ff517377dc8b47d5fed
megaturbo <megaturbo@truelevel.io>
2019-07-23 16:43:30 +0200

    Add Atola and PriceFeed address to local.json

--- migrations/3_deploy_atola.js
+++ migrations/3_deploy_atola.js
@@ -1,8 +1,12 @@
+const fs = require('fs')
+const path = require('path')
 const Atola = artifacts.require('Atola')
 const PriceFeed = artifacts.require('PriceFeed')
+const CONFIG = path.resolve(__dirname, '../config')
+const LOCAL_CONFIG = path.join(CONFIG, 'local.json')
 const config = (process.env.NODE_ENV === 'production')
   ? require('../config/ropsten.json')
-  : require('../config/local.json')
+  : require(LOCAL_CONFIG)
 
 if (!config) { throw new Error('Missing config file') }
 
@@ -14,6 +18,15 @@ module.exports = async (deployer) => {
   await deployer.deploy(Atola, baseToken, baseExchange);
   const atola = await Atola.deployed();
   const priceFeed = await deployer.deploy(PriceFeed, atola.address);
+
+  config.ATOLA = atola.address;
+  config.PRICEFEED = priceFeed.address;
+
+  fs.writeFileSync(
+    LOCAL_CONFIG,
+    JSON.stringify(config, undefined, 2),
+    'utf-8'
+  )
 
   // assuming for now that supportedTokens holds exchange addresses
   atola.addToken(baseExchange);
