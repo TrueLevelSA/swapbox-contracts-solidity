846754bf54a6abf242a773d2049cae3c3811c138
megaturbo <megaturbo@truelevel.io>
2019-07-30 16:06:08 +0200

    Fix get correct balance test

--- test_ts/pricefeed.ts
+++ test_ts/pricefeed.ts
@@ -29,19 +29,15 @@ contract('PriceFeed', (accounts) => {
 
   it('get correct balances', async () => {
     const balances = await priceFeed.methods.getBalances().call();
-
-    const addresses = balances[0].map(address => address.toString());
-    const tokenBalances = balances[1];
-    const ethBalances = balances[2].map(
-      ethBalance => fromWei(ethBalance, "ether")
-    );
+    const tokenBalances = balances[1].map(tokenBalance => fromWei(tokenBalance, 'ether'));
+    const ethBalances = balances[2].map(ethBalance => fromWei(ethBalance, "ether"));
 
     // TODO: use a config file for default setup of liquidity
-    assert.equal(tokenBalances[0], '1000', 'tokenBalance is wrong in baseToken');
-    assert.equal(tokenBalances[1], '1000', 'tokenBalance is wrong in scndToken');
+    assert.equal(tokenBalances[0], '15', 'tokenBalance is wrong in baseToken');
+    assert.equal(tokenBalances[1], '15', 'tokenBalance is wrong in scndToken');
 
-    assert.equal(ethBalances[0], '1', 'ethBalances is wrong for baseToken');
-    assert.equal(ethBalances[1], '1', 'ethBalances is wrong for scndToken');
+    assert.equal(ethBalances[0], '15', 'ethBalances is wrong for baseToken');
+    assert.equal(ethBalances[1], '15', 'ethBalances is wrong for scndToken');
   });
 
   it('check buyPrice is smaller than sellPrice', async () => {
