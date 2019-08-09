42c6fa715ffb8e6980a60c6265f4a583779d6274
megaturbo <megaturbo@truelevel.io>
2019-08-09 16:07:40 +0200

    Add machine to allowed machine in migration and add funds to Atola

--- migrations/3_deploy_atola.js
+++ migrations/3_deploy_atola.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const path = require('path')
 const Atola = artifacts.require('Atola')
 const PriceFeed = artifacts.require('PriceFeed')
+const TokenXchf = artifacts.require('XCHF')
 const CONFIG = path.resolve(__dirname, '../config')
 const LOCAL_CONFIG = path.join(CONFIG, 'local.json')
 const config = (process.env.NODE_ENV === 'production')
@@ -14,10 +15,11 @@ const baseToken = config.BASE_TOKEN
 const baseExchange = config.UNISWAP_EXCHANGE
 const secondExchange = config.UNISWAP_EXCHANGE_SCND
 
-module.exports = async (deployer) => {
+module.exports = async (deployer, network, accounts) => {
   await deployer.deploy(Atola, baseToken, baseExchange);
   const atola = await Atola.deployed();
   const priceFeed = await deployer.deploy(PriceFeed, atola.address);
+  const tokenXchf = await TokenXchf.at(baseToken)
 
   config.ATOLA = atola.address;
   config.PRICEFEED = priceFeed.address;
@@ -31,4 +33,13 @@ module.exports = async (deployer) => {
   // assuming for now that supportedTokens holds exchange addresses
   await atola.addToken(baseExchange);
   await atola.addToken(secondExchange);
+
+  // in dev mode create an account for the machine
+  if (network === 'development') {
+    const value = "1000000000000000000000"
+    await atola.addMachine(accounts[2]);
+    await tokenXchf.deposit({from: accounts[0], value: value})
+    await tokenXchf.transfer(config.ATOLA, value, {from: accounts[0]})
+    // await tokenXchf.approve(config.UNISWAP_EXCHANGE, "1000000000000000000000", {from: accounts[1]})
+  }
 };
