522839e8c2c22ddb8eb666b84bb7896ad1938811
megaturbo <megaturbo@truelevel.io>
2019-08-23 20:25:04 +0200

    Add license headers, license file and set license in package.json

--- contracts/PriceFeed.sol
+++ contracts/PriceFeed.sol
@@ -1,3 +1,19 @@
+// Swap-box
+// Copyright (C) 2019  TrueLevel SA
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 // need to figure out how to return the data (cant return array of struct which would be much more elegant than returning an array where for example the first element is token, second is input balance, third is output balance and then next token etc :/)
 pragma solidity ^0.5.8;
 
