fc3ca76f479692e7d4a9208382f887772c871562
megaturbo <megaturbo@truelevel.ch>
2021-04-27 14:56:16 +0200

    refactor: remove dead code

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -18,7 +18,6 @@ import { ethers } from "hardhat";
 import { deploy } from "./deploy_utils";
 
 async function main() {
-  // console.log("Starting deployment on network id: " + ethers.provider.network.chainId);
   const [deployer] = await ethers.getSigners()
   const deployment = await deploy(deployer);
   console.log("" + deployment);
